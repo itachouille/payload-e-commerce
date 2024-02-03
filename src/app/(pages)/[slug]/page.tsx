bd9244eeb56fa1e40cde3d8e02835581b0217c99
Anthony David <anthony.david34@sfr.fr>
2024-02-03 17:52:31 +0100

    products filter

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -11,6 +11,7 @@ import { Blocks } from '../../_components/Blocks'
 import { Gutter } from '../../_components/Gutter'
 import { Hero } from '../../_components/Hero'
 import { generateMeta } from '../../_utilities/generateMeta'
+
 // Payload Cloud caches all files through Cloudflare, so we don't need Next.js to cache them as well
 // This means that we can turn off Next.js data caching and instead rely solely on the Cloudflare CDN
 // To do this, we include the `no-cache` header on the fetch requests used to get the data for this page
@@ -21,6 +22,7 @@ export const dynamic = 'force-dynamic'
 
 import Categories from '../../_components/Categories'
 import Promotion from '../../_components/Promotion'
+
 import classes from './index.module.scss'
 
 export default async function Page({ params: { slug = 'home' } }) {
